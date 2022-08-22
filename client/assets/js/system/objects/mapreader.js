c920819880b2b9aa7a1d96308f6f564407c631c4
Steven Boyd <20269544+PressingButtons@users.noreply.github.com>
2022-08-22 09:13:36 -0500

    Validated current driving code

---
+++ client/assets/js/system/objects/mapreader.js
@@ -0,0 +1,13 @@
+export default class MapReader extends Core.Objects.ListenerObject {
+
+    this.#tiles = { };
+
+    constructor( ) {
+
+    }
+
+    async readImage(image) {
+        
+    }
+
+}
